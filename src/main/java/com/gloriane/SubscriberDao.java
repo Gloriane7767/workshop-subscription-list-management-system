cfa2978647fab6d60e03798e84f3cb475e916142
Glory Ngum Foncha <gloriane7767@gmail.com>
2026-01-14 10:23:47 +0100

    create a subscriberDao class

---
+++ src/main/java/com/gloriane/SubscriberDao.java
@@ -0,0 +1,15 @@
+package com.gloriane;
+
+// Implementation to save subscriber to database
+public class SubscriberDao {
+   public void save(Subscriber subscriber) {
+    }
+
+    // Implementation to find all subscribers from database
+   public void findByAll() {
+    }
+
+    // Implementation to find subscriber by ID from database
+     public void findById(int id) {
+    }
+}
