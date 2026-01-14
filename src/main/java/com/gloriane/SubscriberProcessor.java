7c004918a4026cc9f3f5cb210ff09e8133188854
Glory Ngum Foncha <gloriane7767@gmail.com>
2026-01-14 12:52:00 +0100

    update subscriber logic.

--- src/main/java/com/gloriane/SubscriberProcessor.java
+++ src/main/java/com/gloriane/SubscriberProcessor.java
@@ -5,22 +5,25 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class SubscriberProcessor {
-    public static void applyToMatching(
-            findSubscribers(List<Subscriber> list, Predicate<Subscriber> rule) {
+    public static List<Subscriber>
+    findSubscribers(List<Subscriber> list, Predicate<Subscriber> rule) {
         List<Subscriber> result = new ArrayList<>();
         for (Subscriber subscriber : list) {
             if (rule.test(subscriber)) {
                 result.add(subscriber);
-
             }
         }
+        return result;
     }
-
+   public static List<Subscriber>
     applyToMatching(List<Subscriber> list, Predicate<Subscriber> rule, SubscriberAction action) {
+        List<Subscriber> result = new ArrayList<>();
         for (Subscriber subscriber : list) {
             if (rule.test(subscriber)) {
-                action.perform(subscriber);
+                action.run(subscriber);
+                result.add(subscriber);
             }
         }
+        return result;
     }
 }
