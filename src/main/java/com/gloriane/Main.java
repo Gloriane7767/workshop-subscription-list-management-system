098cbb1965478bca95d2c37ed3138260627b54d5
Glory Ngum Foncha <gloriane7767@gmail.com>
2026-01-14 13:28:36 +0100

    update list in the Main class

--- src/main/java/com/gloriane/Main.java
+++ src/main/java/com/gloriane/Main.java
@@ -6,13 +6,13 @@ import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        List<Subscriber> subscribers = new ArrayList<>();
-        subscribers.add(new Subscriber(1, "alice@example.com", Plan.FREE));
-        subscribers.add(new Subscriber(2, "bob@example.com", Plan.BASIC));
-        subscribers.add(new Subscriber(3, "charlie@example.com", Plan.PRO));
-        subscribers.add(new Subscriber(4, "diana@example.com", Plan.BASIC));
-        subscribers.add(new Subscriber(5, "eve@example.com", Plan.PRO));
-        System.out.println(subscribers);
+        SubscriberDao dao = new SubscriberDao();
+        dao.save(new Subscriber(1, "alice@example.com", Plan.FREE, true, 0));
+        dao.save(new Subscriber(2, "bob@example.com", Plan.BASIC, true, 1));
+        dao.save(new Subscriber(3, "charlie@example.com", Plan.PRO, true, 5));
+        dao.save(new Subscriber(4, "diana@example.com", Plan.BASIC, true, 0));
+        dao.save(new Subscriber(5, "eve@example.com", Plan.PRO, true, 2));
+        System.out.println(dao.findByAll());
     }
 // Rules using lambda expressions for filtering
 }
